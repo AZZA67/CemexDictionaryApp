aa64279f8dfbfb252e53fe9c2a8114ca021a446f
AZZA67 <azzabahaa99@gmail.com>
2022-07-02 14:58:11 +0200

    Templete+Authentication

---
+++ CemexDictionaryApp/Controllers/AccountController.cs
@@ -0,0 +1,111 @@
+﻿using CemexDictionaryApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using CemexDictionaryApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CemexDictionaryApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        private DBContext dbcontext;
+        public AccountController(UserManager<ApplicationUser> _userManager
+        , SignInManager<ApplicationUser> _signInManager
+        , RoleManager<IdentityRole> _roleManager
+        , DBContext _DBContext)
+        {
+            this.signInManager = _signInManager;
+            this.userManager = _userManager;
+            this.roleManager = _roleManager;
+            this.dbcontext = _DBContext;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var UserModel =
+                await userManager.FindByEmailAsync(user.Email);
+
+
+
+                if (UserModel != null)
+                {
+                   var result =
+                    await signInManager.PasswordSignInAsync
+                    (UserModel, user.Password, user.RememberMe, false);
+                    //Check user role to add Logging Record As Reciptionist
+                   
+                    return RedirectToAction("HomePage", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+                }
+
+                }
+            
+            return View(user);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email,
+                    PasswordHash=model.Password,
+                };
+
+                var result = await userManager.CreateAsync(user,user.PasswordHash);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("HomePage", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(model);
+        }
+
+    }
+}
--- CemexDictionaryApp/Startup.cs
+++ CemexDictionaryApp/Startup.cs
@@ -1,5 +1,8 @@
+using CemexDictionaryApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,9 @@ namespace CemexDictionaryApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            string connectionString = Configuration.GetConnectionString("Cemex_Dictionary");
+            services.AddDbContext<DBContext>(c => c.UseSqlServer(connectionString));
+            services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<DBContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,15 +45,25 @@ namespace CemexDictionaryApp
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                    name: "Login",
+                    pattern: "{controller=Account}/{action=Login}/{Id?}");
+            
+            
+                    endpoints.MapControllerRoute(name: "default",
+             pattern: "{controller=Home}/{action=HpmePage}/{id?}");
+                
+            }
+            );
         }
     }
 }
+
+
+
+
